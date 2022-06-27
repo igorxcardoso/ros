953519c46f9470b163226afca13e12b4bd311d56
Igor Cardoso <iygorsilva@gmail.com>
2022-06-27 18:50:17 -0300

    ros creation and settings up

---
+++ activity1/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/igor/projects/ros/activity1/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/igor/projects/ros/activity1/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/igor/projects/ros/activity1/catkin_ws/devel;/opt/ros/melodic' != '' else []

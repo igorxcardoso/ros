7748df5ec88a1d2713298a81943c9cfc10e6587f
Igor Cardoso <iygorsilva@gmail.com>
2022-06-27 21:52:23 -0300

    publisher and subscriber in python

--- activity1/catkin_ws/build/CMakeFiles/Makefile.cmake
+++ activity1/catkin_ws/build/CMakeFiles/Makefile.cmake
@@ -18,6 +18,8 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/home/igor/projects/ros/activity1/catkin_ws/src/CMakeLists.txt"
   "/home/igor/projects/ros/activity1/catkin_ws/src/beginner_tutorials/CMakeLists.txt"
   "/home/igor/projects/ros/activity1/catkin_ws/src/beginner_tutorials/package.xml"
+  "/home/igor/projects/ros/activity1/catkin_ws/src/beginner_tutorials/scripts/listener.py"
+  "/home/igor/projects/ros/activity1/catkin_ws/src/beginner_tutorials/scripts/talker.py"
   "/opt/ros/melodic/share/catkin/cmake/../package.xml"
   "/opt/ros/melodic/share/catkin/cmake/all.cmake"
   "/opt/ros/melodic/share/catkin/cmake/assert.cmake"
--- activity1/catkin_ws/build/catkin_generated/order_packages.py
+++ activity1/catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/igor/projects/ros/activity1/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/igor/projects/ros/activity1/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/igor/projects/ros/activity1/catkin_ws/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
